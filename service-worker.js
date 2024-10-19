7e093155a03ddbf60819b9c5336fc8f155da09eb
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-19 18:17:45 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,43 @@
-const CACHE_NAME = 'n-pontes-locacoes-v1';
+const CACHE_NAME = 'n-pontes-locacoes-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/manifest.json'
+    './index.html',
+    './styles.css',
+    './script.js',
+    './manifest.json',
+    './icon.png' // Adicione outros arquivos que você deseja armazenar em cache
 ];
 
-self.addEventListener('install', function(event) {
+// Instalação do service worker
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Arquivos em cache');
+            .then((cache) => {
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', function(event) {
-    const cacheWhitelist = [CACHE_NAME];
+// Ativação do service worker
+self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
+    );
+});
+
+// Interceptação de requisições
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
     );
 });
