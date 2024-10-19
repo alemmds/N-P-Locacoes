2e8c55ff22e7512fa2e8d1125ad8f9ae1c23ab59
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-19 18:45:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,54 @@
+// Nome do cache
 const CACHE_NAME = 'n-pontes-locacoes-cache-v1';
+// Arquivos para cachear
 const urlsToCache = [
-    './index.html',
-    './styles.css',
-    './script.js',
-    './manifest.json',
-    './icon.png' // Adicione outros arquivos que você deseja armazenar em cache
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png', // Se houver ícones de PWA
+    '/icons/icon-512x512.png'
 ];
 
-// Instalação do service worker
-self.addEventListener('install', (event) => {
+// Evento de instalação do Service Worker
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
+            .then(function (cache) {
+                console.log('Cache aberto');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-// Ativação do service worker
-self.addEventListener('activate', (event) => {
+// Evento de busca
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Se o recurso estiver em cache, usa-o
+                if (response) {
+                    return response;
+                }
+                // Caso contrário, faz a requisição à rede
+                return fetch(event.request);
+            })
+    );
+});
+
+// Evento de ativação e atualização de cache
+self.addEventListener('activate', function (event) {
+    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
+        caches.keys().then(function (cacheNames) {
             return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
-    );
-});
-
-// Interceptação de requisições
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
     );
 });
