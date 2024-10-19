799a37e0c3d2ad8f66f8644ece80840dac3e7d7f
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-19 19:48:53 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,33 +10,19 @@ const urlsToCache = [
 // Instalando o Service Worker e cacheando arquivos estáticos
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Interceptando as requisições e respondendo com o cache
+// Buscando recursos do cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Atualizando o cache e removendo os antigos
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
