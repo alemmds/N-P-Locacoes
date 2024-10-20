1e6778ae6a80349aaef17b74c83c801fede6a4be
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-19 21:55:40 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
+        caches.open('n-pontes-locacoes').then(cache => {
             return cache.addAll([
-                '/',
                 '/index.html',
-                '/styles.css',
+                '/html.css',
                 '/script.js',
                 '/manifest.json',
-                '/images/icon.png' // Exemplo de imagem que pode ser cacheada
+                '/assets/logo.png' // Se você tiver um logo, adicione aqui
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
