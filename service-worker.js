02e746b7fe5b73d5a7751ddc1aeaa13d0fd72cce
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-20 00:39:19 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,20 @@
-const CACHE_NAME = 'n-pontes-locacoes-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './css/styles.css',
-    './js/script.js',
-    './manifest.json'
-];
-
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('n-pontes-cache').then(cache => {
+            return cache.addAll([
+                'index.html',
+                'styles.css',
+                'script.js',
+                'manifest.json'
+            ]);
+        })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
