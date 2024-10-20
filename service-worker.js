b929b670d5dbedfeb063bed9d0d58078b050b0f0
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-20 00:19:30 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('n-pontes-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/script.js',
-                '/manifest.json'
-            ]);
+const cacheName = 'npontes-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/js/script.js',
+    '/images/icon-192.png',
+    '/images/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(assets);
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
         })
     );
 });
