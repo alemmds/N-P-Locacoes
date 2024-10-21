12560135d81055e8833001dd926ad270d92c5455
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-20 22:01:49 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,45 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = 'n-pontes-locacoes-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/images/icons/icon-192x192.png', // Inclua os ícones e outros recursos importantes
+    '/images/icons/icon-512x512.png'
+];
+
+// Instalação do Service Worker e armazenamento no cache
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('n-pontes-locacoes').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/script.js',
-                '/manifest.json'
-            ]);
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('Arquivos armazenados em cache');
+            return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+// Interceptação de solicitações para servir os arquivos em cache
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
+        })
+    );
+});
+
+// Atualização do cache quando uma nova versão é ativada
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
