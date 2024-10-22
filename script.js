442675b8eba6b2f9b829ecc493ac841e894aec2b
Alexandre Mendes <84242222+alemmds@users.noreply.github.com>
2024-10-22 20:56:25 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -285,6 +285,14 @@ function deleteItem(type, index) {
     }
 }
 
+function toggleDetails(detailsId) {
+    var details = document.getElementById(detailsId);
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
 // Ativar a exibição inicial das máquinas
 showList('maquinas');
 
